85ec347ae6bf4c1fd8d90cf843fded47e368a197
Christian Bazaz <cjb329@scarletmail.rutgers.edu>
2024-09-30 16:50:51 -0400

    Added cs50 tutorial

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 # first do git clone https
 # cd into the repo
-# start coding in vscode
-
+# start coding file in vscode
+# git add file
+# git commit -m "message"
+# git status to check
+# finally do git push
 print("Hello, World!")
+
+# git status to check what has been changed
+# add + commit -> git commit -am "Added CS50 tutorial"
