4611e73eb88d083bb458b3e4be637eacec6ba60c
Chris Baz <televisionvalorant@gmail.com>
2024-09-30 16:54:03 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,1 @@
-# first do git clone https
-# cd into the repo
-# start coding file in vscode
-# git add file
-# git commit -m "message"
-# git status to check
-# finally do git push
 print("Hello, World!")
-
-# git status to check what has been changed
-# add + commit -> git commit -am "Added CS50 tutorial"
